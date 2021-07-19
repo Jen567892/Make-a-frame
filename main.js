ad7f1d3f01b04c7a754f48a6c7d9103949283c5b
Jen567892 <82474362+Jen567892@users.noreply.github.com>
2021-07-19 18:06:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(400,400);
+    canvas.position(110,300);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    circle(50,50,80);
+    stroke(255,0,0);
+    fill(255,0,0);
+
+
+
+    fill(0,128,0);
+    circle(0,128,0);
+    rect(90,460,420,20);
+
+    rect(90,460,420,20);
+}
